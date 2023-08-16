5754c449a78e1602dca6df5617efa13240d72bb9
Bogita Brian <bogitabrian@gmail.com>
2023-08-16 23:01:42 +0300

    Added JavaScript functions and event listeners to the event booking app

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const eventSelect = document.getElementById('event');
+
+let ticketPrice = +eventSelect.value;
+
+//update total and count
+
+function updateSelectedCount() {
+
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//movie select event
+
+eventSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+container.addEventListener('click', (e) => {
+
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected');
+    }
+
+    updateSelectedCount();
+
+
+});
